d925e4888633bafb6c00a11f67522de9f6460657
Petyo <petyomarov@gmail.com>
2020-12-22 23:03:17 +0200

    feat: styling sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar/index';
+import Sidebar from './components/Sidebar/index';
 
 // eslint-disable-next-line arrow-body-style
 const App = () => {
   return (
     <Router>
+      <Sidebar />
       <Navbar />
     </Router>
   );

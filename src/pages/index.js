c94569086833759e25076b9495b872d5b4ac5200
Petyo <petyomarov@gmail.com>
2020-12-30 00:49:22 +0200

    feat: add services section

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import {
   homeObjTwo,
   homeObjThree,
 } from '../components/InfoSection/Data';
+import Services from '../components/Services';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,6 +24,7 @@ const Home = () => {
       <HeroSection />
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
+      <Services />
       <InfoSection {...homeObjThree} />
     </>
   );
